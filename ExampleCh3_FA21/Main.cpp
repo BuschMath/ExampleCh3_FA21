c268631e25b1ae9cc40e4731b0d4e5532955d458
David Busch <buschdr@gmail.com>
2021-09-10 10:09:42 -0500

    Value returning function

--- ExampleCh3_FA21/Main.cpp
+++ ExampleCh3_FA21/Main.cpp
@@ -2,16 +2,21 @@
 
 using namespace std;
 
+float r = 3.5;
+float h = 5;
+const float pi = 3.1415926536;
+
+float Calculate();
+
 int main()
 {
-	float r = 3.5;
-	float h = 5;
-	const float pi = 3.1415926536;
-
-	float v = pi * r * r * h;
-
 	cout << "The volume of your cylinder with r=" << r << " and h=" << h;
-	cout << " is: " << v << endl << endl;
+	cout << " is: " << Calculate() << endl << endl;
 
 	return 0;
+}
+
+float Calculate()
+{
+	return pi * r * r * h;
 }
