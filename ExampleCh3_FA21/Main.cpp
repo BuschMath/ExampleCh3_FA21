8417125d4e53b73a800c7e8442f7bc71f45a0d76
David Busch <buschdr@gmail.com>
2021-09-10 10:17:13 -0500

    Arg list

--- ExampleCh3_FA21/Main.cpp
+++ ExampleCh3_FA21/Main.cpp
@@ -2,21 +2,22 @@
 
 using namespace std;
 
-float r = 3.5;
-float h = 5;
-const float pi = 3.1415926536;
-
-float Calculate();
+float Calculate(float r_, float h_);
 
 int main()
 {
+	float r = 3.5;
+	float h = 5;
+
 	cout << "The volume of your cylinder with r=" << r << " and h=" << h;
-	cout << " is: " << Calculate() << endl << endl;
+	cout << " is: " << Calculate(r, h) << endl << endl;
 
 	return 0;
 }
 
-float Calculate()
+float Calculate(float r_, float h_)
 {
-	return pi * r * r * h;
+	const float pi = 3.1415926536;
+
+	return pi * r_ * r_ * h_;
 }
